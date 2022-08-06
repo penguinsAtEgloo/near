0a4ca35408cc922af65e914fbfefcf4955a4ebbc
sinandsean <sinandsean@gmail.com>
2022-08-06 10:46:41 +0900

    feat: install headless dialog and save dialog

--- src/pages/DrawingPage.tsx
+++ src/pages/DrawingPage.tsx
@@ -1,10 +1,15 @@
 import React, { useCallback, useState } from 'react';
 import Button from '../components/Button';
+import SaveDialog from './SaveDialog';
 
 function DrawingPage() {
   const [isSaveMode, setSaveMode] = useState(false);
   const openSaveMode = useCallback(() => setSaveMode(true), []);
   const closeSaveMode = useCallback(() => setSaveMode(false), []);
+
+  const [showSaveDialog, setShowSaveDialog] = useState(false);
+  const openSaveDialog = useCallback(() => setShowSaveDialog(true), []);
+  const closeSaveDialog = useCallback(() => setShowSaveDialog(false), []);
   return (
     <div className="fixed inset-0 flex flex-col">
       <div
@@ -12,6 +17,7 @@ function DrawingPage() {
         onPointerDown={closeSaveMode}
       >
         <div>그리는 화면</div>
+        <SaveDialog isOpen={showSaveDialog} onClose={closeSaveDialog} />
       </div>
       <div className="flex h-20 space-x-10 justify-center items-center">
         {isSaveMode ? (
@@ -19,14 +25,14 @@ function DrawingPage() {
             <Button>링크복사</Button>
             <Button>인스타</Button>
             <Button>페이스북</Button>
-            <Button></Button>
+            <Button onClick={openSaveDialog}>저장</Button>
           </>
         ) : (
           <>
             <Button>펜</Button>
             <Button>지우개</Button>
             <Button>undo</Button>
-            <Button onClick={openSaveMode}>저장</Button>
+            <Button onClick={openSaveMode}>공유</Button>
           </>
         )}
       </div>
