9d33eadda1ba896859040e0748d6d228ff731228
sinandsean <sinandsean@gmail.com>
2022-09-02 00:12:08 +0900

    revert: timer

--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -1,38 +1,33 @@
 import clsx from 'clsx';
-import React, { useState, useEffect, useCallback } from 'react';
-import Button from '../ui/Button';
+import React, { useState, useEffect } from 'react';
 
 function Timer({ className }: { className?: string }): React.ReactElement {
-  const [seconds, setSeconds] = useState<number>(10);
-  const [isStart, setIsStart] = useState<boolean>(false);
-
-  const start = useCallback(() => {
-    if (!isStart) {
-      setIsStart(true);
-    }
-  }, [isStart, setIsStart]);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(5);
 
   useEffect(() => {
-    if (isStart) {
-      const countdown = setInterval(() => {
-        if (seconds > 0) {
-          setSeconds(seconds - 1);
-        }
-        if (seconds === 0) {
+    const countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
           //0분 0초일때
           clearInterval(countdown);
           alert('시간이 종료되었습니다.');
+        } else {
+          // x분 0초가 될때
+          setMinutes(minutes - 1);
+          setSeconds(59);
         }
-      }, 1000);
-      return () => clearInterval(countdown);
-    }
-  }, [isStart, seconds]);
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
 
   return (
     <div className={clsx(className)}>
-      {Math.floor(seconds / 60)}:
-      {seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60}
-      <Button onClick={start}>시작</Button>
+      {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
     </div>
   );
 }
