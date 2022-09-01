077ec71c722fc7ad88c0250495d83ae5aa3b61b1
JiHee Han <g2hhh2ee@gmail.com>
2022-09-01 23:45:23 +0900

    feat: add export button

---
+++ src/icons/Download.tsx
@@ -0,0 +1,18 @@
+import type { ReactElement } from 'react';
+import React from 'react';
+
+const Download: React.FC<React.SVGProps<SVGSVGElement>> = (): ReactElement => (
+  <svg
+    width="33"
+    height="33"
+    viewBox="0 0 33 33"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M26.125 16.5V26.125H6.875V16.5H4.125V26.125C4.125 27.6375 5.3625 28.875 6.875 28.875H26.125C27.6375 28.875 28.875 27.6375 28.875 26.125V16.5H26.125ZM17.875 17.4213L21.4363 13.8738L23.375 15.8125L16.5 22.6875L9.625 15.8125L11.5637 13.8738L15.125 17.4213V4.125H17.875V17.4213Z"
+      fill="white"
+    />
+  </svg>
+);
+export default Download;
