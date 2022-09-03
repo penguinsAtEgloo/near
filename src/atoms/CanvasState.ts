7413c34255ae650d7654482be13a6a5202be88f8
sinandsean <sinandsean@gmail.com>
2022-09-04 00:37:02 +0900

    chore: main page

--- src/atoms/CanvasState.ts
+++
@@ -1,7 +0,0 @@
-import CanvasDraw from 'react-canvas-draw';
-import { atom } from 'recoil';
-
-export const canvasState = atom<CanvasDraw | null>({
-  key: 'canvasState',
-  default: null,
-});
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,18 +1,23 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import sample from '../assets/sample.png';
+import Face from '../icons/Face';
 
 function MainPage(): React.ReactElement {
   return (
-    <div className="fixed inset-0 flex flex-col justify-center items-center space-y-4 bg-gray-200">
-      <img src={sample} />
-      <div className="absolute top-48 p-4 bg-white">
-        친구야 내 초상화를 그려줘
+    <div className="fixed inset-0 flex flex-col justify-center items-center space-y-5 bg-white">
+      <div className="space-y-1.5">
+        <div className="font-extrabold text-3xl text-center">
+          친구야.. 이게 나라고?
+        </div>
+        <div className="font-normal text-xl text-center">
+          3분 안에 그리는 내 친구
+        </div>
       </div>
-      <Link className="absolute bottom-48" to={'/pages/draw'}>
+      <Face color="black" />
+      <Link to={'/pages/draw'}>
         <button className="px-4 py-2 bg-gray-800 text-white">
-          초상화 그리러 가기 &gt;
+          그림 그리기 Start!
         </button>
       </Link>
     </div>
