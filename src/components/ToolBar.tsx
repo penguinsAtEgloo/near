3e3092bf5b5a35a131f1c808ee33531b422b2ca1
haewoniee <hhan9243@gmail.com>
2022-09-04 00:03:47 +0900

    fix: drawing-size

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useCallback, useState } from 'react';
 import Brush from '../icons/Brush';
 import Palette from '../icons/Palette';
@@ -167,3 +168,174 @@ function ToolBar({
 }
 
 export default ToolBar;
+=======
+import React, { useCallback, useState } from 'react';
+import Brush from '../icons/Brush';
+import Palette from '../icons/Palette';
+import ClearAll from '../icons/ClearAll';
+import Undo from '../icons/Undo';
+import CanvasDraw from 'react-canvas-draw';
+import ClearDialog from '../dialog/ClearDialog';
+import ColorButton from './ColorButton';
+import BrushButton from './BrushButton';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { penColorState } from '../atoms/PenColor';
+import { erasedLinesState } from '../atoms/ErasedLines';
+import { linesState } from '../atoms/Lines';
+
+const TOOL_MODE = { color: 'color', width: 'width' } as const;
+type ToolMode = typeof TOOL_MODE[keyof typeof TOOL_MODE];
+
+const COLOR_SET = {
+  white: '#FFFFFF',
+  black: '#000000',
+  red: '#FF0000',
+  yellow: '#FFF500',
+  blue: '#001AFF',
+};
+
+function ToolBar({
+  canvasDraw,
+  onProceed,
+}: {
+  canvasDraw: CanvasDraw | null;
+  onProceed: () => void;
+}): React.ReactElement {
+  const [toolMode, setToolMode] = useState<ToolMode | null>(null);
+
+  const [showClearDialog, setShowClearDialog] = useState(false);
+  const closeClearDialog = useCallback(() => setShowClearDialog(false), []);
+  const requestClear = useCallback(() => setShowClearDialog(true), []);
+  const [erasedLines, setErasedLines] = useRecoilState(erasedLinesState);
+  const lines = useRecoilValue(linesState);
+
+  const clear = useCallback(() => {
+    setShowClearDialog(false);
+    setErasedLines([]);
+    canvasDraw?.clear();
+  }, [canvasDraw, setErasedLines]);
+
+  const undo = useCallback(() => {
+    if (!canvasDraw) return;
+    const lineStr: string | undefined = canvasDraw.getSaveData();
+    if (lineStr) {
+      const lineObj = JSON.parse(lineStr);
+      const lines: never[] = lineObj.lines;
+      const newErased: never[] = [...erasedLines, lines[lines.length - 1]];
+      setErasedLines(newErased);
+    }
+    canvasDraw?.undo();
+  }, [canvasDraw, erasedLines, setErasedLines]);
+
+  const redo = useCallback(() => {
+    if (!canvasDraw) return;
+    if (erasedLines.length === 0) return;
+    const lastErased = erasedLines[erasedLines.length - 1];
+    const curLines = JSON.parse(lines);
+    curLines.lines.push(lastErased);
+    setErasedLines(erasedLines.slice(0, -1));
+    canvasDraw.loadSaveData(JSON.stringify(curLines), true);
+  }, [canvasDraw, erasedLines, lines, setErasedLines]);
+
+  const [penColor, setPenColor] = useRecoilState(penColorState);
+  const setColorValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setPenColor(value);
+  };
+
+  const handleWidthButton = useCallback(() => {
+    if (toolMode) {
+      setToolMode(null);
+      return;
+    }
+    setToolMode('width');
+  }, [toolMode]);
+
+  const handleColorButton = useCallback(() => {
+    if (toolMode) {
+      setToolMode(null);
+      return;
+    }
+    setToolMode('color');
+  }, [toolMode]);
+
+  return (
+    <div className="w-[320px] flex px-11 py-4 space-x-7 items-center rounded-2xl bg-black">
+      {toolMode !== 'color' && (
+        <>
+          <button
+            className="flex space-x-6"
+            type="button"
+            onClick={handleWidthButton}
+          >
+            <Brush />
+          </button>
+          {toolMode === 'width' && (
+            <div className="w-full flex justify-between items-center">
+              <BrushButton size={1} />
+              <BrushButton size={2} />
+              <BrushButton size={4} />
+              <BrushButton size={7} />
+              <BrushButton size={10} />
+            </div>
+          )}
+        </>
+      )}
+      {toolMode !== 'width' && (
+        <>
+          <button
+            className="flex space-x-6 items-center"
+            type="button"
+            onClick={handleColorButton}
+          >
+            <Palette />
+          </button>
+          {toolMode === 'color' && (
+            <div className="w-full flex justify-between">
+              <ColorButton color={COLOR_SET.white} />
+              <ColorButton
+                className="border border-white"
+                color={COLOR_SET.black}
+              />
+              <ColorButton color={COLOR_SET.red} />
+              <ColorButton color={COLOR_SET.yellow} />
+              <ColorButton color={COLOR_SET.blue} />
+              <label className="relative flex cursor-pointer">
+                <div
+                  className="w-3.5 h-3.5 rounded-full bg-gradient-to-b from-red-600 to-blue-600"
+                  style={{ backgroundColor: penColor }}
+                />
+                <input
+                  className="invisible w-0 h-0 bg-white"
+                  type="color"
+                  onChange={setColorValue}
+                />
+              </label>
+            </div>
+          )}
+        </>
+      )}
+      {!toolMode && (
+        <>
+          <button type="button" onClick={requestClear}>
+            <ClearAll />
+          </button>
+          <button type="button" onClick={undo}>
+            <Undo />
+          </button>
+          <button className="w-6 h-6 text-white" type="button" onClick={redo}>
+            &gt;
+          </button>
+          <ClearDialog
+            isOpen={showClearDialog}
+            onClose={closeClearDialog}
+            onClear={clear}
+          />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default ToolBar;
+>>>>>>> 70b1591 (fix: drawing-size)
--- src/pages/DrawingPage.tsx
+++ src/pages/DrawingPage.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useCallback, useState, useRef, useEffect } from 'react';
 import CanvasDraw from 'react-canvas-draw';
 import LoadImageModal from '../components/LoadImageModal';
@@ -232,3 +233,239 @@ function DrawingPage(): React.ReactElement {
 }
 
 export default DrawingPage;
+=======
+import React, { useCallback, useState, useRef, useEffect } from 'react';
+import CanvasDraw from 'react-canvas-draw';
+import LoadImageModal from '../components/LoadImageModal';
+import WebCamModal from '../components/WebCamModal';
+import ToolBar from '../components/ToolBar';
+
+import Camera from '../icons/Camera';
+import Check from '../icons/Check';
+import Download from 'icons/Download';
+import Share from 'icons/Share';
+import Opacity from 'icons/Opacity';
+
+import { useRecoilValue, useSetRecoilState, useRecoilState } from 'recoil';
+import { penWidthState } from '../atoms/PenWidth';
+import { penColorState } from '../atoms/PenColor';
+import { backImageState } from '../atoms/BackImage';
+import { imageSourceState } from '../atoms/ImageSource';
+import { linesState } from '../atoms/Lines';
+import { erasedLinesState } from '../atoms/ErasedLines';
+import Timer from './Timer';
+import { Link } from 'react-router-dom';
+import { drawingState } from '../atoms/DrawingState';
+import { historyState } from '../atoms/HistoryState';
+
+function DrawingPage(): React.ReactElement {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isSaveMode, setSaveMode] = useState(false);
+  const openSaveMode = useCallback(() => setSaveMode(true), []);
+  const closeSaveMode = useCallback(() => setSaveMode(false), []);
+
+  const penColor = useRecoilValue(penColorState);
+  const penWidth = useRecoilValue(penWidthState);
+
+  const backImage = useRecoilValue(backImageState);
+  const [canvasRef, setCanvasRef] = useState<CanvasDraw | null>(null);
+  const [drawing, setDrawing] = useRecoilState(drawingState);
+  const setHistory = useSetRecoilState(historyState);
+
+  const copyURL = useCallback(() => {
+    if (!canvasRef) return;
+    const canvas = document.querySelector(
+      '.CanvasDraw canvas:nth-child(2)'
+    ) as HTMLCanvasElement;
+    const imageURL = canvas.toDataURL('image/png');
+    setDrawing(imageURL);
+    window.navigator.clipboard.writeText(drawing).then(() => {
+      alert('복사 완료!');
+    });
+  }, [canvasRef, drawing, setDrawing]);
+
+  const saveAsPNG = useCallback(() => {
+    if (!canvasRef) return;
+    const canvas = document.querySelector(
+      '.CanvasDraw canvas:nth-child(2)'
+    ) as HTMLCanvasElement;
+    const imageURL = canvas.toDataURL('image/png');
+    setDrawing(imageURL);
+    const canvasImage = document.createElement('a');
+    canvasImage.href = imageURL;
+    canvasImage.download = 'NEAR.png';
+    document.body.appendChild(canvasImage);
+    canvasImage.click();
+
+    setHistory(canvasRef.getSaveData());
+  }, [canvasRef, setDrawing, setHistory]);
+
+  const imageinput = useRef<HTMLInputElement>(null);
+  const [imageSource, setImgSrc] = useRecoilState(imageSourceState);
+  const setLines = useSetRecoilState(linesState);
+  const setErasedLines = useSetRecoilState(erasedLinesState);
+
+  const [showLoadImageModal, setShowLoadImageModal] = useState(false);
+  const closeLoadImageModal = useCallback(() => {
+    setShowLoadImageModal(false);
+  }, []);
+
+  const [showWebCamModal, setShowWebCamModal] = useState(false);
+  const closeWebCamModal = useCallback(() => setShowWebCamModal(false), []);
+
+  const [opacity, setOpacity] = useState<number>(0.5);
+
+  const onSelectFile = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files && e.target.files.length > 0) {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+          setImgSrc(reader.result?.toString() || '');
+          e.target.value = '';
+        });
+        reader.readAsDataURL(e.target.files[0]);
+      }
+    },
+    [setImgSrc]
+  );
+
+  const loadImage = useCallback(() => {
+    imageinput.current?.click();
+  }, []);
+
+  useEffect(() => {
+    try {
+      document.createEvent('TouchEvent');
+      setIsMobile(true);
+    } catch (e) {
+      setIsMobile(false);
+    }
+  }, [setIsMobile]);
+
+  useEffect(() => {
+    if (imageSource) {
+      setShowLoadImageModal(true);
+    }
+  }, [imageSource]);
+
+  const loadCameraModals = useCallback(() => {
+    if (isMobile) {
+      loadImage();
+    } else {
+      setShowWebCamModal(true);
+    }
+  }, [isMobile, loadImage, setShowWebCamModal]);
+
+  const onChangeOpacity = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setOpacity(parseInt(e.target.value) / 100);
+    },
+    [setOpacity]
+  );
+
+  const onChangeCanvas = useCallback(
+    (e: CanvasDraw) => {
+      if (!canvasRef) return;
+      const lineStr = canvasRef.getSaveData();
+      setLines(lineStr);
+      // 사용자가 직접 쓰는 경우 false, canvasRef.loadSaveData 를 쓸 경우 true
+      const valuesChanged: boolean = (canvasRef as any).valuesChanged;
+      if (!valuesChanged) {
+        setErasedLines([]);
+      }
+    },
+    [canvasRef, setLines, setErasedLines]
+  );
+
+  return (
+    <>
+      <div className="fixed inset-0 flex flex-col">
+        <div className="grow w-full bg-gray-200" onPointerDown={closeSaveMode}>
+          <div className="absolute z-10 top-[295px] left-[45px]">
+            <Opacity />
+          </div>
+          <rect className="absolute box-border bg-black w-[185px] h-[38px] top-[350px] left-[-40px] border-solid border-1 shadow-[0_4px_4px_4px_rgba(0,0,0,0.25)] rounded-[100px] rotate-90" />
+          <input id="slider" type="range" onChange={onChangeOpacity} />
+          {backImage && (
+            <div className="absolute">
+              <img
+                src={backImage}
+                alt="배경이미지"
+                style={{ opacity: opacity, width: '375px', height: '667px' }}
+              />
+            </div>
+          )}
+          <CanvasDraw
+            className="CanvasDraw"
+            ref={(canvasDraw) => {
+              setCanvasRef(canvasDraw);
+            }}
+            onChange={onChangeCanvas}
+            // TODO: 영역크기 계산하여 width, height 넣어주어야 함
+            canvasWidth={375}
+            canvasHeight={667}
+            catenaryColor=""
+            brushColor={penColor}
+            brushRadius={penWidth}
+            lazyRadius={0}
+            hideGrid
+            hideInterface
+            enablePanAndZoom={true}
+            mouseZoomFactor={1}
+            zoomExtents={{ min: 0.33, max: 3 }}
+          />
+          <Timer className="absolute" />
+          <input
+            ref={imageinput}
+            onChange={onSelectFile}
+            type="file"
+            className="invisible"
+            accept="image/*"
+          />
+        </div>
+        <div className="absolute top-4 right-4 flex w-[80px] justify-between">
+          <button type="button" onClick={loadCameraModals}>
+            <Camera />
+          </button>
+          <button type="button" onClick={openSaveMode}>
+            <Check />
+          </button>
+        </div>
+        <div className="flex absolute left-1/2 -translate-x-1/2 bottom-11 space-x-2 justify-center">
+          <div>
+            {isSaveMode ? (
+              <>
+                <button
+                  className="flex items-center justify-center gap-4 w-full h-16 px-4 py-2 bg-black text-white rounded-full align-center"
+                  onClick={copyURL}
+                >
+                  친구에게 공유하기
+                  <Share />
+                </button>
+                <Link to={'/pages/preview'}>
+                  <button
+                    className="flex items-center justify-center gap-4 w-full h-16 px-4 py-2 bg-black text-white rounded-full align-center"
+                    onClick={saveAsPNG}
+                  >
+                    파일 저장하기
+                    <Download />
+                  </button>
+                </Link>
+              </>
+            ) : (
+              <ToolBar canvasDraw={canvasRef} onProceed={openSaveMode} />
+            )}
+          </div>
+        </div>
+      </div>
+      <LoadImageModal
+        isOpen={showLoadImageModal}
+        onClose={closeLoadImageModal}
+      />
+      <WebCamModal isOpen={showWebCamModal} onClose={closeWebCamModal} />
+    </>
+  );
+}
+
+export default DrawingPage;
+>>>>>>> 70b1591 (fix: drawing-size)
