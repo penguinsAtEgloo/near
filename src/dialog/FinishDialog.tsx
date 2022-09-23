fcf95deb8c57b4e97a04a9d608ab2e62362f2136
sinandsean <sinandsean@gmail.com>
2022-09-23 23:51:19 +0900

    chore: change stop process

--- src/dialog/FinishDialog.tsx
+++ src/dialog/FinishDialog.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Dialog, { DialogProps } from '../ui/Dialog';
-import { Link } from 'react-router-dom';
 
-type PauseDialogProps = Pick<DialogProps, 'isOpen' | 'onClose'> & {
+type FinishDialogProps = Pick<DialogProps, 'isOpen' | 'onClose'> & {
   onProceed: () => void;
   isTimeout: boolean;
 };
 
-function PauseDialog({
+function FinishDialog({
   isOpen,
   onClose,
   isTimeout,
   onProceed,
-}: PauseDialogProps): React.ReactElement {
+}: FinishDialogProps): React.ReactElement {
   return (
     <Dialog isOpen={isOpen} onClose={onClose}>
       <div className="flex flex-col pt-3 space-y-4 text-center text-xl divide-y">
@@ -31,15 +30,17 @@ function PauseDialog({
           >
             {isTimeout ? '다시 도전!' : '아니요'}
           </button>
-          <Link className="w-1/2 py-2 text-lg" to={'/pages/preview'}>
-            <button type="button" onClick={onProceed}>
-              {isTimeout ? '저장하기' : '예'}
-            </button>
-          </Link>
+          <button
+            className="w-1/2 py-2 text-lg"
+            type="button"
+            onClick={onProceed}
+          >
+            {isTimeout ? '저장하기' : '예'}
+          </button>
         </div>
       </div>
     </Dialog>
   );
 }
 
-export default PauseDialog;
+export default FinishDialog;
