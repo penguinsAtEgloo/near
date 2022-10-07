abbdcd8c4285b1834f5ce6a13835277b7f5a0ad6
sinandsean <sinandsean@gmail.com>
2022-10-07 22:26:04 +0900

    chore: switch button in clear dialog

--- src/dialog/ClearDialog.tsx
+++ src/dialog/ClearDialog.tsx
@@ -11,11 +11,11 @@ function ClearDialog({ isOpen, onClose, onClear }: ClearDialogProps) {
       <div className="flex flex-col pt-5 space-y-4 text-center divide-y">
         <div className="text-lg font-medium">그림을 초기화할까요?</div>
         <div className="flex justify-evenly divide-x text-lg font-medium">
-          <button className="w-1/2 py-3" type="button" onClick={onClear}>
-            네 삭제할게요
-          </button>
           <button className="w-1/2 py-3" type="button" onClick={onClose}>
             아니요
+          </button>
+          <button className="w-1/2 py-3" type="button" onClick={onClear}>
+            네 삭제할게요
           </button>
         </div>
       </div>
